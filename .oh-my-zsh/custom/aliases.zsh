510b5a784f11066cb28215d6ea22237c27a03516
etanase <eduard.tanase1@gmail.com>
2023-02-16 18:23:01 -0500

    more stuff

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,17 @@
+alias ls='exa --icons --color=always --group-directories-first'
+alias la='exa -a --icons --color=always --group-directories-first'
+alias ll='exa --git -l --icons --color=always --group-directories-first'
+alias lla='exa --git -l -a --icons --color=always --group-directories-first'
+alias lt='exa -TDL1 --icons --color=always --group-directories-first'
+alias ltt='exa -TDL2 --icons --color=always --group-directories-first'
+alias lttt='exa -TDL3 --icons --color=always --group-directories-first'
+alias ltttt='exa -TDL4 --icons --color=always --group-directories-first'
+
+alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+alias ececomp='xxh $USER@ececomp.ecn.purdue.edu +s zsh +hh "~"'
+
+alias gcc='gcc -std=c99 -g -Wall -Wshadow -pedantic -Wvla -Werror'
+alias valgrind='valgrind --tool=memcheck --leak-check=full --verbose'
+
+alias rm='rm -i'
